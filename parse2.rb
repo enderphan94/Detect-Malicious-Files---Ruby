7136a12afc47a34b95e74db565549a0f66db17ce
ender <weintome@gmail.com>
2017-02-28 18:47:09 +0200

    done with sample update

--- parse2.rb
+++ parse2.rb
@@ -1,16 +1,35 @@
 require 'optparse'
 require 'ostruct'
 require 'digest'
+require 'mongo'
+
+Mongo::Logger.logger.level = ::Logger::FATAL
+
+client = Mongo::Client.new(['127.0.0.1:27017'], :database =>'hashSample')
 
 options = OpenStruct.new
 OptionParser.new do |opt|
-  opt.on('-f', '--first_name FIRSTNAME', 'The first name') { |o| options.first_name = o }
-  opt.on('-l', '--last_name LASTNAME', 'The last name') { |o| options.last_name = o }
+  opt.on('-s', '--sample SAMPLE', 'The sample') { |o| options.first_name = o }
 end.parse!
 
-d=options.first_name
-l=options.last_name
+s=options.first_name
 
-digest = Digest::MD5.base64digest(File.read( d ))
-puts "The MD5 digest base64 encoded MD5 is #{digest}."
+digest = Digest::MD5.base64digest(File.read( s ))
+puts "The file hashed is: #{digest}."
+puts "-------------------"
+sample = {:hash => digest}
 
+storage_arr = Array.new
+
+client[:backdoor].find().each do |a|
+	
+	storage_arr.push(a['hash'])
+end
+
+if(storage_arr.include?(digest))
+	puts "The sample is already existed in database!!!"
+else
+	client[:backdoor].insert_one sample
+	puts "Sampled added successfully!!!"
+end
+client.close
