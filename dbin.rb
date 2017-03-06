5ea32fff48d6c1bcd13b61d2d481a777cb65d1e0
ender <weintome@gmail.com>
2017-03-06 15:49:36 +0200

    just changed names

---
+++ dbin.rb
@@ -0,0 +1,62 @@
+require 'optparse'
+require 'ostruct'
+require 'digest'
+require 'mongo'
+
+Mongo::Logger.logger.level = ::Logger::FATAL
+
+client = Mongo::Client.new(['127.0.0.1:27017'], :database =>'hashSample')
+
+options = OpenStruct.new
+OptionParser.new do |opt|
+  opt.on('-i', '--sample else', 'The unhashed sample') { |o| options.sample = o }
+  opt.on('-s', '--show [use "-s show"]', 'Show the sample in database') { |o| options.show = o}
+  opt.on('-h', '--help', 'Displays Help') do
+		puts opt
+		exit
+  end
+end.parse!
+
+i=options.sample
+s=options.show
+if(i)
+	count=0
+	digest = Digest::MD5.base64digest(File.read(i))
+	puts "The file has been hashed is: #{digest}."
+	puts "-------------------"
+
+	sample = {:hash => digest}
+
+	storage_arr = Array.new
+
+	client[:backdoor].find().each do |a|
+	
+		storage_arr.push(a['hash'])
+	end
+
+	if(storage_arr.include?(digest))
+		puts "The sample is already existed in database!!!"
+	else
+		client[:backdoor].insert_one sample
+		puts "Samples added successfully!!!"
+	end
+
+	client[:backdoor].find().each do |a|
+		count= count+1
+	end
+
+	puts "There are #{count} sample(s) is(are) available. You can use -s to show the data"
+end
+if(s=="show") 
+
+	puts "The hashed samples in database\n"
+        puts "-----------------------------------------"
+
+	client[:backdoor].find().each do |a|
+		#puts "\n"
+		puts "|\t#{a['hash']}\t|"
+		puts "-----------------------------------------"
+	end
+end
+
+client.close
