d2225dabebf6659e3cfa1a818bbffa9d237e97d5
ender <weintome@gmail.com>
2017-03-07 10:09:18 +0200

    rubocop

--- dbin.rb
+++ dbin.rb
@@ -5,58 +5,58 @@ require 'mongo'
 
 Mongo::Logger.logger.level = ::Logger::FATAL
 
-client = Mongo::Client.new(['127.0.0.1:27017'], :database =>'hashSample')
+client = Mongo::Client.new(['127.0.0.1:27017'], database: 'hashSample')
 
 options = OpenStruct.new
 OptionParser.new do |opt|
   opt.on('-i', '--sample else', 'The unhashed sample') { |o| options.sample = o }
-  opt.on('-s', '--show [use "-s show"]', 'Show the sample in database') { |o| options.show = o}
+  opt.on('-s', '--show [use "-s show"]', 'Show the sample in database') { |o| options.show = o }
   opt.on('-h', '--help', 'Displays Help') do
-		puts opt
-		exit
+      puts opt
+    exit
   end
 end.parse!
 
-i=options.sample
-s=options.show
-if(i)
-	count=0
-	digest = Digest::MD5.base64digest(File.read(i))
-	puts "The file has been hashed is: #{digest}."
-	puts "-------------------"
+i = options.sample
+s = options.show
+if (i)
+   count = 0
+  digest = Digest::MD5.base64digest(File.read(i))
+  puts "The file has been hashed is: #{digest}."
+  puts '-------------------'
 
-	sample = {:hash => digest}
+  sample = { hash: digest }
 
-	storage_arr = Array.new
+  storage_arr = []
 
-	client[:backdoor].find().each do |a|
-	
-		storage_arr.push(a['hash'])
-	end
+  client[:backdoor].find.each do |a|
+  
+     storage_arr.push(a['hash'])
+  end
 
-	if(storage_arr.include?(digest))
-		puts "The sample is already existed in database!!!"
-	else
-		client[:backdoor].insert_one sample
-		puts "Samples added successfully!!!"
-	end
+  if (storage_arr.include?(digest))
+     puts 'The sample is already existed in database!!!'
+  else
+     client[:backdoor].insert_one sample
+    puts 'Samples added successfully!!!'
+  end
 
-	client[:backdoor].find().each do |a|
-		count= count+1
-	end
+  client[:backdoor].find.each do |_a|
+     count+ = 1
+  end
 
-	puts "There are #{count} sample(s) is(are) available. You can use -s to show the data"
+  puts "There are #{count} sample(s) is(are) available. You can use -s to show the data"
 end
-if(s=="show") 
+if (s == 'show')
 
-	puts "The hashed samples in database\n"
-        puts "-----------------------------------------"
+   puts "The hashed samples in database\n"
+ puts '-----------------------------------------'
 
-	client[:backdoor].find().each do |a|
-		#puts "\n"
-		puts "|\t#{a['hash']}\t|"
-		puts "-----------------------------------------"
-	end
+  client[:backdoor].find.each do |a|
+    # puts "\n"
+     puts "|\t#{a['hash']}\t|"
+    puts '-----------------------------------------'
+  end
 end
 
 client.close
